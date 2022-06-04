b6196d939fe5770d3aa3404a6c8063a6e437370e
Jawline0700 <wencerscastillo40@gmail.com>
2022-06-04 12:45:22 -0500

    Cambios commit on 'master'

--- js/control.js
+++ js/control.js
@@ -1,12 +1,8 @@
 
 const contenedorProductos = document.getElementById('contenedor-productos')
-
 const contenedorCarrito = document.getElementById('carrito-contenedor')
-
 const botonVaciar = document.getElementById('vaciar-carrito')
-
 const contadorCarrito = document.getElementById('contadorCarrito')
-
 const cantidad = document.getElementById('cantidad')
 const precioTotal = document.getElementById('precioTotal')
 const cantidadTotal = document.getElementById('cantidadTotal')
